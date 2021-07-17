88f2af3ebc307bdfe361fab26eb5dbc221099424
Amani Muhammed <71679579+amanimuhammed@users.noreply.github.com>
2021-07-17 10:39:00 +0530

    Mine

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -106,13 +106,13 @@ async def bot_start(event):
             start_msg = f"Hey! 👤{mention},\
                         \nI am {my_mention}'s assistant bot.\
                         \nYou can contact to my master from here.\
-                        \n\nPowered by [Catuserbot](https://t.me/catuserbot)"
+                        \n\nPowered by [Amani Muhammed](https://t.me/Amani_m_h_d)"
         buttons = [
             (
-                Button.url("Repo", "https://github.com/sandy1709/catuserbot"),
+                Button.url("Master 👨‍💻", "https://t.me/Amani_m_h_d"),
                 Button.url(
-                    "Deploy",
-                    "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack&template=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack",
+                    "Channel 📌",
+                    "https://t.me/My_Test_Botz",
                 ),
             )
         ]
